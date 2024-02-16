f2bd7b4c89e8ca5b293182cc5e6b31849b1ab73b
TheAwakenedBob <107313261+TheAwakenedBob@users.noreply.github.com>
2024-02-16 13:15:16 -0500

    added input args for main

--- SimilaritySearch.java
+++ SimilaritySearch.java
@@ -1,5 +1,16 @@
+import java.io.File;
+
 public class SimilaritySearch {
     public static void main(String args[]) {
+        if (args.length != 2) {
+            System.out.println("Please enter: java SimilaritySearch <fileName> <folderNae>");
+            return;
+        }
+        
+        //get the filename and directory path from cmd
+        String fileName = args[0];
+        String folderName = args[1];
+
         
     }
 }
